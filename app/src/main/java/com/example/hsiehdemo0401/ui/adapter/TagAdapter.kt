8509500a381ae296e8509718e9aa9f91539e97f2
RAY <shyrule789@gmail.com>
2024-04-30 14:53:40 +0800

    display tag ok

--- app/src/main/java/com/example/hsiehdemo0401/ui/adapter/TagAdapter.kt
+++ app/src/main/java/com/example/hsiehdemo0401/ui/adapter/TagAdapter.kt
@@ -3,53 +3,38 @@ package com.example.hsiehdemo0401.ui.adapter
 import android.view.LayoutInflater
 import android.view.ViewGroup
 import androidx.databinding.DataBindingUtil
-import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.Glide
 import com.bumptech.glide.load.resource.bitmap.CenterCrop
 import com.bumptech.glide.load.resource.bitmap.CircleCrop
 import com.example.hsiehdemo0401.R
 import com.example.hsiehdemo0401.databinding.CellModelBinding
+import com.example.hsiehdemo0401.databinding.CellTagBinding
 import com.example.hsiehdemo0401.ui.model.Item
 import com.example.hsiehdemo0401.ui.model.Model
-import com.google.gson.TypeAdapterFactory
 
-class ModelAdapter : RecyclerView.Adapter<ModelAdapter.ModelViewHolder>() {
+class TagAdapter : RecyclerView.Adapter<TagAdapter.ModelViewHolder>() {
 
 
     /* collection of adapter */
-    var list = listOf<Item>()
+    var list = listOf<String>()
         set(value) {
             field = value
             notifyDataSetChanged()
         }
 
 
-    inner class ModelViewHolder(val binding: CellModelBinding) : RecyclerView.ViewHolder(binding.root) {
+    inner class ModelViewHolder(val binding: CellTagBinding) : RecyclerView.ViewHolder(binding.root) {
 
-        fun bind(item: Item) {
-            Glide.with(binding.ivAvatar)
-                .load(item.user.imageUrl)
-                .transform(CenterCrop(), CircleCrop())
-                .into(binding.ivAvatar)
-
-            binding.tvName.text = item.user.nickName
-
-            val tagAdapter = TagAdapter().apply {
-                list = item.tags
-            }
-            binding.tvTags.apply {
-                layoutManager = LinearLayoutManager(binding.root.context, LinearLayoutManager.HORIZONTAL, false)
-                adapter = tagAdapter
-            }
-
+        fun bind(str: String) {
+            binding.tvTag.text = str
         }
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ModelViewHolder {
         return ModelViewHolder(
             DataBindingUtil.inflate(
-                LayoutInflater.from(parent.context), R.layout.cell_model, parent, false
+                LayoutInflater.from(parent.context), R.layout.cell_tag, parent, false
             )
         )
     }

5dd3848a1e0429b727795231dd8aa1ee60e4fa8c
RAY <shyrule789@gmail.com>
2024-04-30 13:49:56 +0800

    change viewPager to horizontal

--- app/src/main/java/com/example/hsiehdemo0401/MainActivity.kt
+++ app/src/main/java/com/example/hsiehdemo0401/MainActivity.kt
@@ -1,33 +1,17 @@
 package com.example.hsiehdemo0401
 
 import android.os.Bundle
-import android.util.Log
 import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
 import androidx.databinding.DataBindingUtil
-import androidx.lifecycle.Lifecycle
-import androidx.lifecycle.MutableLiveData
-import androidx.lifecycle.ViewModelProvider
-import androidx.lifecycle.flowWithLifecycle
-import androidx.lifecycle.lifecycleScope
-import com.example.hsiehdemo0401.data.frag.TAG
 import com.example.hsiehdemo0401.data.pager_adapter.MyPagerAdapter
 import com.example.hsiehdemo0401.data.viewmodel.MyViewModel
 import com.example.hsiehdemo0401.databinding.ActivityMainBinding
 import com.example.hsiehdemo0401.ui.frag.FragOne
 import com.example.hsiehdemo0401.ui.frag.FragTwo
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.delay
-import kotlinx.coroutines.flow.MutableSharedFlow
-import kotlinx.coroutines.flow.MutableStateFlow
-import kotlinx.coroutines.flow.asStateFlow
-import kotlinx.coroutines.flow.collect
-import kotlinx.coroutines.flow.collectLatest
-import kotlinx.coroutines.flow.flow
-import kotlinx.coroutines.flow.flowOn
-import kotlinx.coroutines.launch
 
 const val TAG = "qq"
+
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
@@ -45,7 +29,6 @@ class MainActivity : AppCompatActivity() {
         viewModel.randomLive.observe(this) {
             binding.textView.text = "$it"
         }
-
 
 
     }
